e018589b7e2b8ec3e1a35090a26faa8b44379051
Brian <jacklevybrsg@gmail.com>
2021-05-13 11:15:57 -0500

    npm init config

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const messages = [
+    "Nicolas",
+    "Renato",
+    "Oscar",
+    "Laura"
+]
+
+const randomMsg = () => {
+    const message = messages[Math.floor(Math.random()* messages.length)]
+    console.log(message)
+}
+
+module.exports = { randomMsg }
